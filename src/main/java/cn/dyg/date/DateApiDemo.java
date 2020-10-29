75006e8d15f8fc6957b4c79357ec253d4c2e4e7f
YinggangDong <believerD@aliyun.com>
2020-10-29 18:55:44 +0800

    feat:Java 8 新特性：新的日期API

---
+++ src/main/java/cn/dyg/date/DateApiDemo.java
@@ -0,0 +1,17 @@
+package cn.dyg.date;
+
+import java.time.LocalDate;
+
+/**
+ * DateApiDemo 类是 新的日期API
+ *
+ * @author dongyinggang
+ * @date 2020-10-29 18:50
+ **/
+public class DateApiDemo {
+
+    public static void main(String[] args) {
+        LocalDate localDate = LocalDate.of(2020,10,29);
+
+    }
+}
