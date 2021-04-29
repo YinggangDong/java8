89673426b39bd11e7cf8348ad22c262920a3faf6
YinggangDong <believerD@aliyun.com>
2021-04-29 10:16:20 +0800

    perf:computeIfAbsent方法限定的注释

--- src/main/java/cn/dyg/functionalinterface/ComputeIfAbsentTest.java
+++ src/main/java/cn/dyg/functionalinterface/ComputeIfAbsentTest.java
@@ -16,9 +16,14 @@ public class ComputeIfAbsentTest {
     public static void main(String[] args) {
         Map<String,Object> map = new HashMap<>();
         map.put("key2","value1");
-        //返回的value值一定是 Value 类的子类,要不然 put 不进去
+        //出参被范式 (? extend V) 限定为是 map 的 value 类型 Object 的子类,要不然 put 不进去
+        //入参被范式 (? super K) 限定为是 map 的 key 类型 String 的父类
+        //但实际入参 key 始终是 key 类型 String 的变量
+        //尽管限定支持 String 的父类,但实际还是直接被规定为了 String
         map.computeIfAbsent("key1",key ->new ArrayList<>());
-        map.computeIfAbsent("key1", new Function<String, Object>() {
+        //将入参设为 Object也可以,但前面的 key 已经被限定为 String 了
+        //实际执行  mappingFunction.apply(key) 时，key 就是 String
+        map.computeIfAbsent("key1", new Function<Object, Object>() {
             /**
              * Applies this function to the given argument.
              *
@@ -26,7 +31,7 @@ public class ComputeIfAbsentTest {
              * @return the function result
              */
             @Override
-            public Object apply(String s) {
+            public Object apply(Object s) {
                 return new ArrayList<>();
             }
         });
